d5052f08f8afcdf3fd3b87c6992c5a4c52c1a5ec
Guillermo Cala <35546879+Guillermocala@users.noreply.github.com>
2018-11-19 09:45:56 -0500

    EJERCICIO PENDIENTE A RESOLVER

---
+++ EJERCICIO2_matrices_funciones.cpp
@@ -0,0 +1,9 @@
+/*
+*elaborar un programa que sume los elementos de cada una de las filas y de cada una de las columnas de una matriz; el resultado de cada suma se almacenara en la ultuma posicion de la fila o columna correspondiente
+*ademas la suma total de todos los elementos de la matriz se almacenara en el elemento de la esquina inferior derecha de la matriz
+1	7	0
+5	6	0
+6	4	0
+7	3	0
+0	0	0
+*/
