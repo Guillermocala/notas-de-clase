9e9d406594b37a34e420f215894fae1bc874a4e4
Guillermo Cala <35546879+Guillermocala@users.noreply.github.com>
2018-11-21 10:02:42 -0500

    subiendo

--- EJERCICIO2_matrices_funciones.cpp
+++ EJERCICIO2_matrices_funciones.cpp
@@ -1,47 +1,93 @@
 /*
 *elaborar un programa que sume los elementos de cada una de las filas y de cada una de las columnas de una matriz; 
-*el resultado de cada suma se almacenara en la ultima posicion de la fila o columna correspondiente
+*el resultado de cada suma se almacenara en la ultuma posicion de la fila o columna correspondiente
 *ademas la suma total de todos los elementos de la matriz se almacenara en el elemento de la esquina inferior derecha de la matriz
-1	7	0
-5	6	0
-6	4	0
-7	3	0
-0	0	0
+*	1	7	0
+*	5	6	0
+*	6	4	0
+*	7	3	0
+*	0	0	0
+*	
+*	C = C + 1
+*	F = F + 1
 */
 #include <iostream>
 using namespace std;
-void insertaVector (int vector, int Nfilas);
-void imprimeVector (int vector, int Nfilas);
+int filas, columnas;
+const int NFilas = 10;
+const int NColumnas = 10;
+float Matriz[NFilas][NColumnas];
+void LeerMatriz (float Tabla[][NColumnas], int CFilas, int CColumnas);
+void SumaFilas (float Tabla[][NColumnas], int *CFilas, int *CColumnas); 
+void SumaColumnas (float Tabla[][10], int *CFilas, int *CColumnas); 
+void MostrarMatriz (float Tabla[][10], int CFilas, int CColumnas);
 
-int main ()
+int main()
 {
-	cout << "Ingrese el numero de filas: ";
+	cout << "Ingrese el numero de las filas (maximo 9): ";
 	cin >> filas;
-	int numeros[filas][10];
-	insertaVector(numeros, filas);
-	imprimeVector()
+	cout << "Ingrese el numero de columnas (maximo 9): ";
+	cin >> columnas;
+	LeerMatriz (Matriz, filas, columnas);
+	SumaFilas (Matriz, &filas, &columnas);
+	SumaColumnas (Matriz, &filas, &columnas);
+	MostrarMatriz (Matriz, filas, columnas);
 	
 	
-	
-	system ("pause");
+	system("pause");
 	return 0;
 }
-void insertaVector (int vector, int Nfilas)
+void LeerMatriz (float Tabla[][NColumnas], int CFilas, int CColumnas)
 {
-	for (int j = 0; j < Nfilas; j++)
+	int i, j;
+	for (i = 0; i < CFilas; i++) /*leer las matrices*/
 	{
-		for (int k = 0; k < 10; k++)
+		for (j = 0; j < CColumnas; j++)
 		{
-			cout << "Ingrese el valor para la posicion: [" << j + 1 << "] [" << k + 1 << "] :";
-			cin >> vector[j][k];
+			cout << "Ingrese el valor en la posicion [" << i + 1 << "] [" << j + 1 << "]: ";
+			cin >> Tabla[i][j];
 		}
 	}
 }
-void imprimeVector (int vector, int Nfilas)
+void SumaFilas (float Tabla[][NColumnas], int *CFilas, int *CColumnas)
 {
-	for (int i = 0; i < Nfilas; i++)
+	int i, j;
+	for (i = 0; i < *CFilas; i++)
 	{
-		cout << "El elemento [" << i + 1 << "] es: " << vector[i] << endl;
-		
+		float suma = 0;
+		for (j = 0; j < *CColumnas; j++)
+		{
+			suma = suma + Tabla[i][j];
+		}
+		Tabla[i][j] = suma; /*guarda en la ultima columna de esa fila*/
+		suma = 0;
+	}
+	*CColumnas = *CColumnas + 1;
+}
+void SumaColumnas (float Tabla[][10], int *CFilas, int *CColumnas)
+{
+	int i, j;
+	for (i = 0; i < *CColumnas; i++)
+	{
+		float suma = 0;
+		for (j = 0; j < *CFilas; j++)
+		{
+			suma = suma + Tabla[i][j];
+		}
+		Tabla[i][j] = suma; /*guarda en la ultima fila de esa columna*/
+		suma = 0;
+	}
+	*CFilas = *CFilas + 1;
+}
+void MostrarMatriz (float Tabla[][10], int CFilas, int CColumnas)
+{
+	int i, j;
+	for (i = 0; i < CFilas; i++)
+	{
+		for (j = 0; j < CColumnas; j++)
+		{
+			cout << "[" << i + 1 << "][" << j + 1 << "]" << Tabla[i][j] << "\t";
+		}
+		cout << endl;
 	}
 }
--- EJE_matrices_funciones.cpp
+++ EJE_matrices_funciones.cpp
@@ -5,18 +5,18 @@
 #include <iostream>
 using namespace std;
 int filas;
-void EntrarMatrizNca (float matriz[][10], int NFilas); /*solo se van a aceptar datos NUMERICOS*/
-void SumaFilas (float matriz[][10], int NFilas, float Sfila[]);
-void SumaColumnas (float matriz[][10], int NFilas, float SCol[]);
-void imprimeVector (float vector[], int NElem); /*nos sirve para imprimir cualquier vector*/
+void EntrarMatrizNca (float matriz [][10], int NFilas); /*solo se van a aceptar datos NUMERICOS*/
+void SumaFilas (float matriz [][10], int NFilas, float Sfila []);
+void SumaColumnas (float matriz [][10], int NFilas, float SCol []);
+void imprimeVector (float vector [], int NElem); /*nos sirve para imprimir cualquier vector*/
 
 int main()
 {
 	cout << "Entre la cantidad de filas: ";
 	cin >> filas;
-	float Datos[filas][10];
-	float fila[filas];
-	float columna[10];
+	float Datos [filas][10];
+	float fila [filas];
+	float columna [10];
 	EntrarMatrizNca (Datos, filas);
 	SumaFilas (Datos, filas, fila);
 	SumaColumnas (Datos, filas, columna);
@@ -28,7 +28,7 @@ int main()
 	system ("pause");
 	return 0;
 }
-void EntrarMatrizNca (float matriz[][10], int NFilas)
+void EntrarMatrizNca (float matriz [][10], int NFilas)
 {
 	int i, j;
 	for (i = 0; i < NFilas; i++) /*leer las matrices*/
@@ -36,37 +36,37 @@ void EntrarMatrizNca (float matriz[][10], int NFilas)
 		for (j = 0; j < 10; j++)
 		{
 			cout << "Ingrese la posicion " << "[" << i+1 << "] [" << j+1 << "] : ";
-			cin >> matriz[i][j];
+			cin >> matriz [i][j];
 		}
 	}
 }
-void SumaFilas (float matriz[][10], int NFilas, float SFila[])
+void SumaFilas (float matriz [][10], int NFilas, float SFila [])
 {
 	float suma = 0;
 	for (int i = 0; i < NFilas; i++)
 	{
 		for (int k = 0; k < 10; k++)
 		{
-			suma = suma + matriz[i][k];
+			suma = suma + matriz [i][k];
 		}
-		SFila[i] = suma; /*guarda en el vector la suma de esa fila*/
+		SFila [i] = suma; /*guarda en el vector la suma de esa fila*/
 		suma = 0;
 	}
 }
-void SumaColumnas (float matriz[][10], int NFilas, float SCol[])
+void SumaColumnas (float matriz [][10], int NFilas, float SCol [])
 {
 	float suma = 0;
 	for (int m = 0; m < 10; m++)
 	{
 		for (int p = 0; p < NFilas; p++)
 		{
-			suma = suma + matriz[p][m];
+			suma = suma + matriz [p][m];
 		}
-		SCol[m] = suma; /*guarda en el vector la suma de esa columna*/
+		SCol [m] = suma; /*guarda en el vector la suma de esa columna*/
 		suma = 0;
 	}	
 }
-void imprimeVector (float vector[], int NElem)
+void imprimeVector (float vector [], int NElem)
 {
 	for (int i = 0; i < NElem; i++)
 	{
