787f5d889466e21802c3c256408763fc268c91c9
Guillermocala <guillermo.cala7@gmail.com>
2018-11-27 21:04:49 -0500

    terminado

--- EJERCICIO2_matrices_funciones.cpp
+++ EJERCICIO2_matrices_funciones.cpp
@@ -2,11 +2,11 @@
 *elaborar un programa que sume los elementos de cada una de las filas y de cada una de las columnas de una matriz; 
 *el resultado de cada suma se almacenara en la ultuma posicion de la fila o columna correspondiente
 *ademas la suma total de todos los elementos de la matriz se almacenara en el elemento de la esquina inferior derecha de la matriz
-*	1	7	0
-*	5	6	0
-*	6	4	0
-*	7	3	0
-*	0	0	0
+*	1	7	0	=== 1	7	8
+*	5	6	0	=== 5	6	11
+*	6	4	0	=== 6	4	10
+*	7	3	0	=== 7	3	10
+*	0	0	0	=== 19	20	39
 *	
 *	C = C + 1
 *	F = F + 1
@@ -22,7 +22,7 @@ void SumaFilas (float Tabla[][NColumnas], int *CFilas, int *CColumnas);
 void SumaColumnas (float Tabla[][10], int *CFilas, int *CColumnas); 
 void MostrarMatriz (float Tabla[][10], int CFilas, int CColumnas);
 
-int main()
+int main ()
 {
 	cout << "Ingrese el numero de las filas (maximo 9): ";
 	cin >> filas;
@@ -32,9 +32,7 @@ int main()
 	SumaFilas (Matriz, &filas, &columnas);
 	SumaColumnas (Matriz, &filas, &columnas);
 	MostrarMatriz (Matriz, filas, columnas);
-	
-	
-	system("pause");
+	system ("pause");
 	return 0;
 }
 void LeerMatriz (float Tabla[][NColumnas], int CFilas, int CColumnas)
@@ -72,9 +70,9 @@ void SumaColumnas (float Tabla[][10], int *CFilas, int *CColumnas)
 		float suma = 0;
 		for (j = 0; j < *CFilas; j++)
 		{
-			suma = suma + Tabla[i][j];
+			suma = suma + Tabla[j][i];
 		}
-		Tabla[i][j] = suma; /*guarda en la ultima fila de esa columna*/
+		Tabla[j][i] = suma; /*guarda en la ultima fila de esa columna*/
 		suma = 0;
 	}
 	*CFilas = *CFilas + 1;
