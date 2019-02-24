a4131edcc320bd41055caf0877d448815c104045
Guillermo <guillermo.cala7@gmail.com>
2019-02-24 10:25:22 -0500

    maqueta corrected

--- estructura_de_datos_1/maqueta.cpp
+++ estructura_de_datos_1/maqueta.cpp
@@ -1,154 +1,315 @@
-#include <stdio.h>
 #include <stdlib.h>
-#include <conio.h>
+#include <iostream>
+#include <ncurses.h>
 #include <string.h>
+using namespace std;
 
-
- struct nodoa {
-        
-        int info;
-        nodoa *sig;
-     
-    
+struct nodoa
+{
+     int info;
+     nodoa *sig;
 };
-
-
-
 nodoa *ptr=NULL, *r,*p,*k,*h, *j;
-
 nodoa* InsertaCabeza(nodoa *p,int xinfo);
 nodoa* InsertaCola(nodoa *p,int xinfo);
 nodoa* Buscarllave( nodoa *p, int elem);
 nodoa* Eliminacion(nodoa *p,int elem);
+nodoa* Elimrep(nodoa *p, int elem);
+nodoa* Rotar(nodoa *p, int n);
 void Mostrar(nodoa *p);
-
 int menu (void);
- 
-int main(int argc, char*argv[]){
-    system ("CLS");
-    int sw=1, i, j;
-    long opcion;
-    do{
-    switch (menu()){
-    case 1:
-    	 system ("CLS");
-    	 printf ("Ingrese La Informacion");
-         scanf ("%d", &i);
-         ptr=InsertaCabeza(ptr,i);
-       break;
-    case 2:
-       	 system ("CLS");
-    	 printf ("Ingrese La Informacion");
-         scanf ("%d", &i);	
-         ptr=InsertaCola(ptr,i);
-       break;
-     case 3:
-        system ("CLS");
-    	 	
-		getch();
-       break;
-     case 4:
-     	system("cls");
-     	
-       break;
-	 case 5:
-	 	
-       break;
-       
-       case 6:
-    
-       	break;
-       	
-       case 0:
-       	system ("CLS");
-         printf ("Press any key to exit...");
+
+int main(int argc, char*argv[])
+{
+   system ("CLS");
+   int sw=1, i, j, x, q;
+   long opcion;
+   do
+   {
+      switch (menu())
+      {
+         case 1:
+            system ("CLS");
+            printf ("Ingrese La Informacion: ");
+            scanf ("%d", &i);
+            ptr=InsertaCabeza(ptr,i);
+            break;
+         case 2:
+            system ("CLS");
+            printf ("Ingrese La Informacion: ");
+            scanf ("%d", &i);
+            ptr=InsertaCola(ptr,i);
+            break;
+         case 3:
+            system ("CLS");
+            if(ptr==NULL)
+            {
+               printf("La lista se encuentra vacia, por favor llenarla");
+               cin.get();
+            }
+            else
+            {
+               printf("Ingrese el dato a Buscar: ");
+               scanf("%d", &i);
+               nodoa *q;
+               q=Buscarllave(ptr, i);
+               if(q!=NULL)
+               {
+                  printf("\n\n Se encontro el elemento");
+               }
+               else
+               {
+                  printf("\n\n No se encontro el elemento");
+               }
+            cin.get();
+            break;
+         case 4:
+            system("cls");
+            if(ptr==NULL)
+            {
+               printf("La lista se encuentra vacia, por favor llenarla");
+            }
+            else
+            {
+               printf("Ingrese el dato a eliminar: ");
+               scanf("%d", &i);
+               ptr=Eliminacion(ptr, i);
+            }
+            break;
+         case 5:
+            system("cls");
+            Mostrar(ptr);
+            cin.get();
+            break;
+         case 6:
+            system("cls");
+            if(ptr==NULL)
+            {
+               printf("La lista se encuentra vacia, por favor llenarla");
+            }
+            else
+            {
+               printf("Ingrese el dato a eliminar: ");
+               scanf("%d", &i);
+               ptr=Elimrep(ptr, i);
+            }
+            cin.get();
+            break;
+         case 7:
+            system("cls");
+            printf("Ingrese la cantidad a rotar: ");
+            scanf("%d", &i);
+            ptr = Rotar(ptr, i);
+            cin.get();
+            break;
+         case 0:
+            system ("CLS");
+            printf ("Press any key to exit...");
+            sw=0;
+            break;
+         default:
+            system ("CLS");
+            printf("opcion no valida...por favor intente nuevamente\n");
+            break;
+      }
+   }
+   while(sw);
+   cin.get();
+}
+int menu (void)
+{
+   int opcion;
+   system("CLS");
+   system ("color 0B");
+
+   printf ("\n\n");
+   printf("\t\t\tï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½\n");
+   printf("\t\t\tï¿½     ESTRUCTURA DE DATOS        ï¿½\n");
+   printf("\t\t\tï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½\n");
+   printf("\t\t\tï¿½                                ï¿½\n");
+   printf("\t\t\tï¿½ 1  ->  Insertar Nodos cabeza   ï¿½\n");
+   printf("\t\t\tï¿½                                ï¿½\n");
+   printf("\t\t\tï¿½ 2  ->  Insertar Nodos Cola     ï¿½\n");
+   printf("\t\t\tï¿½                                ï¿½\n");
+   printf("\t\t\tï¿½ 3  ->  Consulta                ï¿½\n");
+   printf("\t\t\tï¿½                                ï¿½\n");
+   printf("\t\t\tï¿½ 4  ->  Eliminacion             ï¿½\n");
+   printf("\t\t\tï¿½                                ï¿½\n");
+   printf("\t\t\tï¿½ 5  ->  Mostrar Lista           ï¿½\n");
+   printf("\t\t\tï¿½                                ï¿½\n");
+   printf("\t\t\tï¿½ 6  ->  Elim. repetidos         ï¿½\n");
+   printf("\t\t\tï¿½                                ï¿½\n");
+   printf("\t\t\tï¿½ 7  ->  Rotar nodos             ï¿½\n");
+   printf("\t\t\tï¿½                                ï¿½\n");
+   printf("\t\t\tï¿½ 0  ->  Salir                   ï¿½\n");
+   printf("\t\t\tï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½\n");
+   printf("\t\t\tï¿½    Elija una opcion...         ï¿½\n");
+   printf("\t\t\tï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½\n");
+
+
+   int sw=1;
+   do
+   {
+      opcion=cin.get();
+      if(opcion>=48 && opcion <=57)
+      {
          sw=0;
-         break;
-     
-     default:
-         system ("CLS");
-         printf("opcion no valida...por favor intente nuevamente\n");
-         break;    
-    }
-    }while(sw);
-
-    
-getch();
-}
-int menu (void){
-int opcion;
-system("CLS");
-system ("color 3A");
-   
-printf ("\n\n");
-printf("\t\t\t²²²²²²²²²²²²²²²²²²²²²²²²²²²²²²²²²²\n");
-printf("\t\t\t²     ESTRUCTURA DE DATOS        ²\n");
-printf("\t\t\t²²²²²²²²²²²²²²²²²²²²²²²²²²²²²²²²²²\n");
-printf("\t\t\t²                                ²\n");
-printf("\t\t\t² 1  ->  Insertar Nodos cabeza   ²\n");
-printf("\t\t\t²                                ²\n");
-printf("\t\t\t² 2  ->  Insertar Nodos Cola     ²\n");
-printf("\t\t\t²                                ²\n");
-printf("\t\t\t² 3  ->  Consulta                ²\n");
-printf("\t\t\t²                                ²\n");
-printf("\t\t\t² 4  ->  Eliminacion             ²\n");
-printf("\t\t\t²                                ²\n");
-printf("\t\t\t² 5  ->  Mostrar Lista           ²\n");
-printf("\t\t\t²                                ²\n"); 
-printf("\t\t\t² 6  ->                          ²\n");
-printf("\t\t\t²                                ²\n");
-printf("\t\t\t²                                ²\n");
-printf("\t\t\t² 0  ->  Salir                   ²\n");
-printf("\t\t\t²²²²²²²²²²²²²²²²²²²²²²²²²²²²²²²²²²\n");
-printf("\t\t\t²    Elija una opcion...         ²\n");
-printf("\t\t\t²²²²²²²²²²²²²²²²²²²²²²²²²²²²²²²²²²\n");
-    
- 
-int sw=1;
-    do {
-    opcion=getch();
-       if (opcion>=48 && opcion <=57){
-       sw=0;
-       }else
-        printf("\n se debe ingresar un entero entre 1-9\n");
-    
-    }while(sw);
-    opcion=opcion-48;
-
+      }
+      else
+      {
+         printf("\n se debe ingresar un entero entre 1-9\n");
+      }
+   }
+   while(sw);
+   opcion=opcion-48;
    return opcion;
-   } 
-     
-nodoa* InsertaCabeza (nodoa *ptr,int xinfo){
-   
-    p= (struct nodoa *) malloc (sizeof (nodoa));
-    p->info = xinfo;
-   
+}
+nodoa* InsertaCabeza (nodoa *ptr,int xinfo)
+{
+   p= (struct nodoa *) malloc (sizeof (nodoa));
+   p->info = xinfo;
+   if(ptr==NULL)
+   {
+      ptr=p;
+      p->sig=NULL;
+   }
+   else
+   {
+      p->sig=ptr;
+      ptr=p;
+   }
+   return ptr;
 }
 
-nodoa* InsertaCola (nodoa *ptr,int xinfo){
-  
+nodoa* InsertaCola (nodoa *ptr,int xinfo)
+{
+   p= (struct nodoa *) malloc (sizeof (nodoa));
+   p->info = xinfo;
+   if(ptr==NULL)
+   {
+      ptr=p;
+      p->sig=NULL;
+   }
+   else
+   {
+      j=ptr;
+      while(j->sig!=NULL)
+      {
+         j=j->sig;
+      }
+      j->sig=p;
+      p->sig=NULL;
+   }
+   return ptr;
+}
+void  Mostrar (nodoa *ptr)
+{
+   system ("CLS");
+   r=ptr;
+   printf("PTR");
+   while(r!=NULL)
+   {
+      printf("[%d]->", r->info);
+      r=r->sig;
+   }
+   printf("NULL");
+   cin.get();
 }
 
-
-void  Mostrar (nodoa *ptr){
-      system ("CLS");
-      r=ptr->sig;
-      while(r!=ptr){
-      	printf("[%d]->", r->info);
-      	r=r->sig;
-	  }
-getch();
+nodoa* Buscarllave(nodoa *ptr,int elem)
+{
+   nodoa *k;
+   k=ptr;
+   while(k!=NULL)
+   {
+      if(k->info==elem)
+      {
+         return k;
+      }
+      else
+      {
+         k=k->sig;
+      }
+   }
+   k=NULL;
+   return k;
 }
 
-nodoa* Buscarllave(nodoa *ptr,int elem){
-
+nodoa* Eliminacion (nodoa *ptr,int elem)
+{
+   j= Buscarllave(ptr, elem);
+   if(j==NULL)
+   {
+      printf("El elemento no se encuentra en la lista, intentalo de nuevo");
+   }
+   else if(j==ptr)
+   {
+      ptr=j->sig;
+      free(j);
+   }
+   else
+   {
+      r=ptr;
+      while(r->sig!=j)
+      {
+         r=r->sig;
+      }
+      r->sig=j->sig;
+      free(j);
+   }
+   return (ptr);
 }
 
-nodoa* Eliminacion (nodoa *ptr,int elem){
-  
+nodoa* Elimrep(nodoa *ptr, int elem)
+{
+   r=ptr;
+   int c=0;
+   while(r!=NULL)
+   {
+      if(r->info!=elem)
+      {
+         r=r->sig;
+      }
+      else if(r==ptr)
+      {
+         ptr=r->sig;
+         free(r);
+         c++;
+         r=ptr;
+      }
+      else
+      {
+         nodoa *j=ptr;
+         j=ptr;
+         while(j->sig!=r)
+         {
+            j=j->sig;
+         }
+         j->sig=r->sig;
+         free(r);
+         c++;
+         r=j;
+      }
+   }
+   printf("El dato se elimino: %d veces", c);
+   return ptr;
 }
 
-
-
-
-
+nodoa* Rotar(nodoa *ptr, int n)
+{
+   printf("Nodos Originales: \n");
+   Mostrar(ptr);
+   for(int i=1; i<=n; i++)
+   {
+      j=ptr;
+      while(j->sig!=NULL)
+      {
+         j=j->sig;
+      }
+      j->sig=ptr;
+      ptr=ptr->sig;
+      j=j->sig;
+      j->sig=NULL;
+   }
+   return ptr;
+}
