0a306d85cbf870a9e24927f75d657d9e16290c52
Guillermo cala <guillermo.cala7@gmail.com>
2019-08-26 15:17:23 -0500

    corrigiendo y añadiendo

---
+++ estructura_de_datos_2/java/arboles/src/datos/ArbolBinEnteros.java
@@ -0,0 +1,147 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package datos;
+//import java.util.Scanner;
+/**
+ *
+ * @author enanv
+ */
+public class ArbolBinEnteros
+{    
+   Arbin crearArbol()
+   {
+      Arbin raiz = new ArbinEnl(1);
+      Arbin i1 = new ArbinEnl(2); /*raiz.enlIzq(new ArbinEnl(2));*/
+      raiz.enlIzq(i1);
+      Arbin i2 = new ArbinEnl(3);
+      raiz.enlDer(i2);
+      raiz.izq().enlIzq(new ArbinEnl(4)); /*enlazar desde la raiz*/
+      raiz.der().enlDer(new ArbinEnl(5));
+      raiz.izq().izq().enlDer(new ArbinEnl(8));
+      raiz.der().der().enlIzq(new ArbinEnl(7));        
+      return raiz;
+   }
+   void preorden(Arbin x)
+   {
+      if(x != null)
+      {
+         System.out.println(" " + x.obtener()); /*se escribe sout y luego TAB*/
+         preorden(x.izq());
+         preorden(x.der());
+      }
+   }
+   void postorden(Arbin x)
+   {
+      if(x != null)
+      {
+         postorden(x.izq());
+         postorden(x.der());
+         System.out.println(" " + x.obtener());
+      }
+   }
+   void inorden(Arbin x)
+   {
+      if(x!=null)
+      {
+         inorden(x.izq());
+         System.out.println(" " + x.obtener());
+         inorden(x.der());
+      }
+   }
+   int peso(Arbin x)
+   {
+      if(x == null)
+      {
+         return 0;      
+      }
+      else
+      {
+         return 1 + peso(x.izq()) + peso(x.der());
+      }
+   }
+   int Mayor(int x, int y)
+   {
+      int result = x > y ? x : y;
+      return result;
+   }
+   int altura(Arbin x)
+   {
+      if(x == null)
+      {
+         return 0;
+      }
+      else
+      {
+         return 1 + Mayor(altura(x.izq()), altura(x.der()));
+      }
+   }
+   boolean buscar(Arbin x, int elem)
+   {
+      if(x != null)
+      {
+         if(x.obtener() == elem)
+         {
+            return true;           
+         }
+         else
+         {
+            return buscar(x.izq(), elem) || buscar(x.der(), elem);
+         }
+      }
+      return false;
+   }
+   void escribaHojas(Arbin x)
+   {
+      if(x != null)
+      {
+         if(x.izq() == null && x.der() == null)
+         {
+            System.out.println(" " + x.obtener());
+         }
+         escribaHojas(x.izq());
+         escribaHojas(x.der());
+      }
+   }
+   int total = 0;
+   void sumaTodos(Arbin x)
+   {
+      if(x != null)
+      {
+         total += x.obtener();
+         sumaTodos(x.izq());
+         sumaTodos(x.der());
+      }
+   }
+   
+   public static void main(String[] args)
+   { /* se escribe psvm y luego preciono tab*/
+      int p, num;
+      ArbolBinEnteros arbol = new ArbolBinEnteros();
+      Arbin raiz = arbol.crearArbol();
+      System.out.println("El arbol en orden inorden...");
+      arbol.inorden(raiz);
+      p = arbol.peso(raiz);
+      System.out.println("El peso del arbol es: " + p);
+      System.out.println("La altura del arbol es: " + arbol.altura(raiz));
+      p = arbol.altura(raiz);
+      System.out.println("El grado del arbol es: " + (p - 1));
+      /*System.out.println("Ingrese el dato a buscar: ");
+      Scanner entrada = new Scanner(System.in);
+      num = entrada.nextInt();
+      if(arbol.buscar(raiz, num))
+      {
+         System.out.println("Dato encontrado");
+      }
+      else
+      {
+         System.out.println("Dato no encontrado");
+      }*/
+      System.out.println("Las hojas son...");
+      arbol.escribaHojas(raiz);
+      arbol.sumaTodos(raiz);      
+      
+   }
+}
