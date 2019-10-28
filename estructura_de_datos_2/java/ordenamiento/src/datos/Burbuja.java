356ed893c0c8c16fe0a5c13058d5162469ebc10d
Guillermo cala <guillermo.cala7@gmail.com>
2019-10-28 14:50:27 -0500

    quiero dormir T.T

--- estructura_de_datos_2/java/ordenamiento/src/datos/Burbuja.java
+++ estructura_de_datos_2/java/ordenamiento/src/datos/Burbuja.java
@@ -5,6 +5,8 @@
  */
 package datos;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
 import javax.swing.JOptionPane;
 
 /**
@@ -12,22 +14,21 @@ import javax.swing.JOptionPane;
  * @author 57300
  */
 public class Burbuja {
-   public int[] generar(int n) {
+   public int[] generarVec(int n) {
       int v[] = new int[n];
       for (int i = 0; i < v.length; i++) {
          v[i] = (int) (Math.random() * 1000);
       }
       return v;
    }
-   public String mostrarvec(int v[]) {
+   public String mostrarVec(int v[]) {
       String lis = "";
-      for (int i = 0; i < v.length; i++) {
-         int j = v[i];
-         lis += j + " - "; 
+      for (int i = 0; i < v.length; i++) {         
+         lis += v[i] + " - "; 
       }
       return lis;
    }
-   public void ordenar(int v[]) {
+   public void ordenarVec(int v[]) {
       int temp;
       for (int i = 0; i < v.length; i++) {
          for (int j = 0; j < (v.length - 1); j++) {
@@ -39,17 +40,115 @@ public class Burbuja {
          }
       }
    }
+   public LinkedList<Integer> generarList(int n) {
+      LinkedList<Integer> lista = new LinkedList<>();
+      for (int i = 0; i < n; i++) {
+         lista.add((int) (Math.random() * 1000));
+      }
+      return lista;
+   }
+   public String mostrarList(LinkedList<Integer> temp) {
+      String lis = "";
+      for (int i = 0; i < temp.size(); i++) {
+         lis += temp.get(i) + " - "; 
+      }
+      return lis;
+   }
+   public void ordenarList(LinkedList<Integer> lista) {
+      int temp;
+      for (int i = 0; i < lista.size(); i++) {
+         for (int j = 0; j < (lista.size() - 1); j++) {
+            if (lista.get(j) > lista.get(j + 1)) {
+               temp = lista.get(j);
+               lista.set(j, lista.get(j + 1));
+               lista.set(j + 1, temp);
+            }
+         }
+      }
+   }
+   public ArrayList<Integer> generarCol(int n) {
+      ArrayList<Integer> lista = new ArrayList<>();
+      for (int i = 0; i < n; i++) {
+         lista.add((int) (Math.random() * 1000));
+      }
+      return lista;
+   }
+   public String mostrarCol(ArrayList<Integer> temp) {
+      String lis = "";
+      for (int i = 0; i < temp.size(); i++) {
+         lis += temp.get(i) + " - "; 
+      }
+      return lis;
+   }
+   public void ordenarCol(ArrayList<Integer> col) {
+      int temp;
+      for (int i = 0; i < col.size(); i++) {
+         for (int j = 0; j < (col.size() - 1); j++) {
+            if (col.get(j) > col.get(j + 1)) {
+               temp = col.get(j);
+               col.set(j, col.get(j + 1));
+               col.set(j + 1, temp);
+            }
+         }
+      }
+   }
    public static void main(String[] args) {
       Burbuja nuevo = new Burbuja();
-      String showMe = "";
-      int vector[] = nuevo.generar(5);
-      showMe += nuevo.mostrarvec(vector);
-      long timeInit = System.nanoTime();
-      nuevo.ordenar(vector);
-      long timeFinal = System.nanoTime();
-      long timeElapsed = timeFinal - timeInit;
-      showMe += "\nOrdenado\n" + nuevo.mostrarvec(vector);
-      showMe += "\nTime elapsed: " + timeElapsed + " Miliseconds";
-      JOptionPane.showMessageDialog(null, showMe);
+      String showMe, tamanio;
+      int sw = 1, tamanio1;
+      String menu = "BUBBLE SORTING METHOD\n1- Vector \n2- List \n3- Collecction \n0- Exit";      
+      do {
+         showMe = "";
+         String opcion = JOptionPane.showInputDialog(menu);
+         int opcion2 = Integer.parseInt(opcion);
+         switch(opcion2)
+         {
+            case 1:
+               tamanio = JOptionPane.showInputDialog("Enter the length: ");
+               tamanio1 = Integer.parseInt(tamanio);
+               int vector[] = nuevo.generarVec(tamanio1);
+               showMe += nuevo.mostrarVec(vector);
+               long timeInit = System.nanoTime();
+               nuevo.ordenarVec(vector);
+               long timeFinal = System.nanoTime();
+               long timeElapsed = timeFinal - timeInit;
+               showMe += "\nOrdered\n" + nuevo.mostrarVec(vector);
+               showMe += "\nTime elapsed: " + timeElapsed + " Nanoseconds";
+               JOptionPane.showMessageDialog(null, showMe);
+               break;
+            case 2:
+               tamanio = JOptionPane.showInputDialog("Enter the length: ");
+               tamanio1 = Integer.parseInt(tamanio);
+               LinkedList<Integer> lista2 = nuevo.generarList(tamanio1);
+               showMe += nuevo.mostrarList(lista2);
+               timeInit = System.nanoTime();
+               nuevo.ordenarList(lista2);
+               timeFinal = System.nanoTime();
+               timeElapsed = timeFinal - timeInit;
+               showMe += "\nOrdered\n" + nuevo.mostrarList(lista2);
+               showMe += "\nTime elapsed: " + timeElapsed + " Nanoseconds";
+               JOptionPane.showMessageDialog(null, showMe);
+               break;
+            case 3:
+               tamanio = JOptionPane.showInputDialog("Enter the length: ");
+               tamanio1 = Integer.parseInt(tamanio);
+               ArrayList<Integer> col = nuevo.generarCol(tamanio1);
+               showMe += nuevo.mostrarCol(col);
+               timeInit = System.nanoTime();
+               nuevo.ordenarCol(col);
+               timeFinal = System.nanoTime();
+               timeElapsed = timeFinal - timeInit;
+               showMe += "\nOrdered\n" + nuevo.mostrarCol(col);
+               showMe += "\nTime elapsed: " + timeElapsed + " Nanoseconds";
+               JOptionPane.showMessageDialog(null, showMe);
+               break;
+            case 0:
+               sw = 0;               
+               break;
+            default:
+               JOptionPane.showMessageDialog(null, "INCORRECT OPTION!");
+               break;
+         }
+      } while(sw != 0);
    }
 }
--- estructura_de_datos_2/java/ordenamiento/src/datos/QuickSort.java
+++ estructura_de_datos_2/java/ordenamiento/src/datos/QuickSort.java
@@ -5,6 +5,8 @@
  */
 package datos;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
 import javax.swing.JOptionPane;
 
 /**
@@ -12,22 +14,21 @@ import javax.swing.JOptionPane;
  * @author 57300
  */
 public class QuickSort {
-   public int[] generar(int n) {
+   public int[] generarVec(int n) {
       int v[] = new int[n];
       for (int i = 0; i < v.length; i++) {
          v[i] = (int) (Math.random() * 1000);
       }
       return v;
    }
-   public String mostrarvec(int v[]) {
+   public String mostrarVec(int v[]) {
       String lis = "";
       for (int i = 0; i < v.length; i++) {
-         int j = v[i];
-         lis += j + " - "; 
+         lis += v[i] + " - "; 
       }
       return lis;
    }
-   public void ordenar(int v[], int ini, int fin) {
+   public void ordenarVec(int v[], int ini, int fin) {
       int m = (ini + fin) / 2;
       int pivot = v[m];
       int i = ini;
@@ -49,24 +50,153 @@ public class QuickSort {
          j--;
       }
       if (ini < j) {
-         ordenar(v, ini, j);
+         ordenarVec(v, ini, j);
       }
       else if (fin > j) {
-         ordenar(v, i, fin);
+         ordenarVec(v, i, fin);
+      }
+   }
+   public LinkedList<Integer> generarList(int n) {
+      LinkedList<Integer> lista = new LinkedList<>();
+      for (int i = 0; i < n; i++) {
+         lista.add((int) (Math.random() * 1000));
+      }
+      return lista;
+   }
+   public String mostrarList(LinkedList<Integer> temp) {
+      String lis = "";
+      for (int i = 0; i < temp.size(); i++) {
+         lis += temp.get(i) + " - "; 
+      }
+      return lis;
+   }
+   public void ordenarList(LinkedList<Integer> lis, int ini, int fin) {
+      int m = (ini + fin) / 2;
+      int pivot = lis.get(m);
+      int i = ini;
+      int j = fin;
+      int temp;
+      while (i <= j) {         
+         while (lis.get(i) < pivot) {            
+            i++;
+         }
+         while (lis.get(j) > pivot) {            
+            j--;
+         }
+         if (i <= j) {
+            temp = lis.get(i);
+            lis.set(i, lis.get(j));
+            lis.set(j, temp);            
+         }
+         i++;
+         j--;
+      }
+      if (ini < j) {
+         ordenarList(lis, ini, j);
+      }
+      else if (fin > j) {
+         ordenarList(lis, i, fin);
+      }
+   }
+   public ArrayList<Integer> generarCol(int n) {
+      ArrayList<Integer> lista = new ArrayList<>();
+      for (int i = 0; i < n; i++) {
+         lista.add((int) (Math.random() * 1000));
+      }
+      return lista;
+   }
+   public String mostrarCol(ArrayList<Integer> temp) {
+      String lis = "";
+      for (int i = 0; i < temp.size(); i++) {
+         lis += temp.get(i) + " - "; 
+      }
+      return lis;
+   }
+   public void ordenarCol(ArrayList<Integer> lis, int ini, int fin) {
+      int m = (ini + fin) / 2;
+      int pivot = lis.get(m);
+      int i = ini;
+      int j = fin;
+      int temp;
+      while (i <= j) {         
+         while (lis.get(i) < pivot) {            
+            i++;
+         }
+         while (lis.get(j) > pivot) {            
+            j--;
+         }
+         if (i <= j) {
+            temp = lis.get(i);
+            lis.set(i, lis.get(j));
+            lis.set(j, temp);            
+         }
+         i++;
+         j--;
+      }
+      if (ini < j) {
+         ordenarCol(lis, ini, j);
+      }
+      else if (fin > j) {
+         ordenarCol(lis, i, fin);
       }
    }
    public static void main(String[] args) {
-      QuickSort nuevo = new QuickSort();
-      String showMe = "";
-      int vector[] = nuevo.generar(5);
-      int fin = vector.length - 1; // se resta una posicion, porque el inicio va en 0
-      showMe += nuevo.mostrarvec(vector);
-      long timeInit = System.nanoTime();
-      nuevo.ordenar(vector, 0, fin);
-      long timeFinal = System.nanoTime();
-      long timeElapsed = timeFinal - timeInit;
-      showMe += "\nOrdenado\n" + nuevo.mostrarvec(vector);
-      showMe += "\nTime elapsed: " + timeElapsed + " Miliseconds";
-      JOptionPane.showMessageDialog(null, showMe);
+      QuickSort nuevo = new QuickSort();     
+      String showMe, tamanio;
+      int sw = 1, tamanio1;
+      String menu = "QUICKSORT SORTING METHOD\n1- Vector \n2- List \n3- Collection \n0- Exit";      
+      do {
+         showMe = "";
+         String opcion = JOptionPane.showInputDialog(menu);
+         int opcion2 = Integer.parseInt(opcion);
+         switch(opcion2)
+         {
+            case 1:
+               tamanio = JOptionPane.showInputDialog("Enter the length: ");
+               tamanio1 = Integer.parseInt(tamanio);
+               int vector[] = nuevo.generarVec(tamanio1);
+               showMe += nuevo.mostrarVec(vector);
+               long timeInit = System.nanoTime();
+               nuevo.ordenarVec(vector, 0, (vector.length - 1));
+               long timeFinal = System.nanoTime();
+               long timeElapsed = timeFinal - timeInit;
+               showMe += "\nOrdered\n" + nuevo.mostrarVec(vector);
+               showMe += "\nTime elapsed: " + timeElapsed + " Nanoseconds";
+               JOptionPane.showMessageDialog(null, showMe);
+               break;
+            case 2:
+               tamanio = JOptionPane.showInputDialog("Enter the length: ");
+               tamanio1 = Integer.parseInt(tamanio);
+               LinkedList<Integer> lista2 = nuevo.generarList(tamanio1);               
+               showMe += nuevo.mostrarList(lista2);
+               timeInit = System.nanoTime();
+               nuevo.ordenarList(lista2, 1, (lista2.size() - 1));
+               timeFinal = System.nanoTime();
+               timeElapsed = timeFinal - timeInit;
+               showMe += "\nOrdered\n" + nuevo.mostrarList(lista2);
+               showMe += "\nTime elapsed: " + timeElapsed + " Nanoseconds";
+               JOptionPane.showMessageDialog(null, showMe);
+               break;
+            case 3:
+               tamanio = JOptionPane.showInputDialog("Enter the length: ");
+               tamanio1 = Integer.parseInt(tamanio);
+               ArrayList<Integer> col = new ArrayList<>();               
+               showMe += nuevo.mostrarCol(col);
+               timeInit = System.nanoTime();
+               nuevo.ordenarCol(col, 1, col.size());
+               timeFinal = System.nanoTime();
+               timeElapsed = timeFinal - timeInit;
+               showMe += "\nOrdered\n" + nuevo.mostrarCol(col);
+               showMe += "\nTime elapsed: " + timeElapsed + " Nanoseconds";
+               JOptionPane.showMessageDialog(null, showMe);
+               break;
+            case 0:
+               sw = 0;               
+               break;
+            default:
+               JOptionPane.showMessageDialog(null, "Dato incorrecto!");
+               break;
+         }
+      } while(sw != 0);
    }
 }
