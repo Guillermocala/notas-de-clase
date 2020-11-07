7a008c81c5fbbcf9bcb5e734ad1486534f18543f
Guillermo Cala <guillermo.cala7@gmail.com>
2020-11-06 23:46:24 -0500

    botones y pantalla bien, falta funcionalidad

--- Poo/Taller 4/src/Principal/Ventana.java
+++ Poo/Taller 4/src/Principal/Ventana.java
@@ -5,12 +5,13 @@
  */
 package Principal;
 
-import java.awt.BorderLayout;
-import java.awt.Button;
-import java.awt.FlowLayout;
+import java.awt.Font;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Arrays;
 import javax.swing.*;
 
 /**
@@ -19,9 +20,8 @@ import javax.swing.*;
  */
 public class Ventana extends JFrame{
    JPanel panelNum;
-   JPanel principal;
    JPanel operaciones;
-   JTextArea panelText;
+   JTextField panelText;
    
    public Ventana(){
       super("Calculadora");                  
@@ -30,8 +30,11 @@ public class Ventana extends JFrame{
       GridBagConstraints constrains = new GridBagConstraints();
       
       //el textarea que sirve de panel de salida
-      panelText = new JTextArea();
-      panelText.setLineWrap(true);
+      panelText = new JTextField();
+      //a continuacion irá la fuente, en negrita y con tamanio de 30
+      panelText.setFont(new Font("SansSerif", 1, 30));
+      panelText.setHorizontalAlignment(JTextField.RIGHT);
+      //panelText.setEnabled(false);  //para que solo entren datos por los botones
       constrains.gridx = 0;   //empieza columna
       constrains.gridy = 0;   //empieza fila
       constrains.gridwidth = 3;  //ocupa columna
@@ -44,11 +47,11 @@ public class Ventana extends JFrame{
       //montaje del panel numerico
       panelNum = new JPanel();
       panelNum.setLayout(new GridLayout(4, 3));
-      for (int i = 7; i < 10; i++) panelNum.add(new Button("" + i));
-      for (int i = 4; i < 7; i++) panelNum.add(new Button("" + i));
-      for (int i = 1; i < 4; i++) panelNum.add(new Button("" + i));
-      panelNum.add(new JButton("0"));
-      panelNum.add(new JButton("."));
+      for (int i = 7; i < 10; i++) newNumButton(String.valueOf(i));
+      for (int i = 4; i < 7; i++) newNumButton(String.valueOf(i));
+      for (int i = 1; i < 4; i++) newNumButton(String.valueOf(i));
+      newNumButton("0");
+      newNumButton(".");
       constrains.gridx = 0; 
       constrains.gridy = 1;
       constrains.gridwidth = 2;
@@ -60,12 +63,12 @@ public class Ventana extends JFrame{
       //montaje del panel de operaciones
       operaciones = new JPanel();
       operaciones.setLayout(new GridLayout(0, 1));
-      operaciones.add(new Button("Erase"));
-      operaciones.add(new Button("/"));
-      operaciones.add(new Button("*"));
-      operaciones.add(new Button("-"));
-      operaciones.add(new Button("+"));
-      operaciones.add(new Button("Intro"));
+      newOperationButton("Erase");
+      newOperationButton("/");
+      newOperationButton("*");
+      newOperationButton("-");
+      newOperationButton("+");
+      newOperationButton("Intro");
       constrains.gridx = 2; 
       constrains.gridy = 1;
       constrains.gridwidth = 1;
@@ -77,6 +80,40 @@ public class Ventana extends JFrame{
       setVisible(true);
    }
    
+   private void newNumButton(String num){
+      JButton button = new JButton();
+      button.setText(num);
+      button.addActionListener(this::verifica);
+      panelNum.add(button);
+   }
+   
+   private void newOperationButton(String operation){
+      JButton button = new JButton();
+      button.setText(operation);
+      button.addActionListener(this::verifica);
+      operaciones.add(button);
+   }
+   private void verifica(ActionEvent e){
+      String opera[] = {".", "/", "*", "-", "+", "Intro"};
+      String data = e.getActionCommand();
+      String aux = panelText.getText(), temp;
+      if(data.equals("Erase")){
+         if (aux.length() > 0) {
+            temp =  aux.substring(0, aux.length() - 1);
+            panelText.setText(temp);
+         }
+         System.out.println("esta pulsando erase");
+      }
+      else if (Arrays.asList(opera).contains(data)) {
+         System.out.println("es una operacion");
+         panelText.setText(aux + e.getActionCommand());
+      }
+      else{
+         System.out.println("es un numero");
+         panelText.setText(aux + e.getActionCommand());
+      }
+      
+   }
    public static void main(String[] args) {
       Ventana app = new Ventana();
    }
