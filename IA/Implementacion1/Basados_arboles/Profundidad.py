a5ad6c35739c092a0ef1d646147eb7403a5d598e
Guillermo Cala <guillermo.cala7@gmail.com>
2022-10-16 13:18:01 -0500

    funciona solo con configuraciones muy sencillas

--- IA/Implementacion1/Basados_arboles/Profundidad.py
+++ IA/Implementacion1/Basados_arboles/Profundidad.py
@@ -2,10 +2,9 @@
 # a sample graph
 import copy
 
-
-initial_config = [[3,4,1],
-                  [2,5,8],
-                  [7,6,0]]
+initial_config = [[1,2,3],
+                  [5,0,6],
+                  [4,7,8]]
           
 goal_config = [[1,2,3],
                [4,5,6],
@@ -17,10 +16,8 @@ def calculatePosition(actual):
             if actual[x][y] == 0:
                 position = (x, y)
                 return position
-                
 
 def moveUp(actual_config):
-    print("moving up...")
     # se usa deepcopy para que no altere la lista original
     res_config = copy.deepcopy(actual_config)
     actual_position = calculatePosition(res_config)
@@ -36,7 +33,6 @@ def moveUp(actual_config):
         return res_config
 
 def moveRight(actual_config):
-    print("moving right...")
     # se usa deepcopy para que no altere la lista original
     res_config = copy.deepcopy(actual_config)
     actual_position = calculatePosition(res_config)
@@ -53,7 +49,6 @@ def moveRight(actual_config):
 
 
 def moveDown(actual_config):
-    print("moving down...")
     # se usa deepcopy para que no altere la lista original
     res_config = copy.deepcopy(actual_config)
     actual_position = calculatePosition(res_config)
@@ -69,7 +64,6 @@ def moveDown(actual_config):
         return res_config
 
 def moveLeft(actual_config):
-    print("moving left...")
     # se usa deepcopy para que no altere la lista original
     res_config = copy.deepcopy(actual_config)
     actual_position = calculatePosition(res_config)
@@ -102,57 +96,27 @@ def expandCurrentState(actual_config):
     if res != -1:
         sucesores.append(res)
 
-    """print("los sucesores son: ")
-    for i in sucesores:
-        for x in i:
-            print(x)
-        print("\n")"""
     return sucesores
     
-
 def dfs_paths(start, goal):
     visitados = set()
     stack = [start]
     while stack:
         path = stack.pop()
-        
-        print("visitados ", visitados)
         print("el path antes de expandir es...")
         for i in path:
             print(i)
 
         if calculatePosition(path) not in visitados:
             sucesores = expandCurrentState(path)
-            print(sucesores)
+            visitados.add(calculatePosition(path))
             for i in sucesores:
-                stack.append(i)
-
-        visitados.add(calculatePosition(path))
-
-
-        if path == goal:
-            print("goal reached")
-            break
-        """l = list(set(expandCurrentState(path)) - set(path))
-        for next in sorted(l, reverse=True):           
-            if next == goal:
-                yield path + [next]
-            else:
-                stack.append((next, path + [next]))"""
+                if i == goal:
+                    print("\t\tGOAL REACHED")
+                    for item in i:
+                        print(item)
+                    return 0
+                else:
+                    stack.append(i)
 
 dfs_paths(initial_config, goal_config)
-
-#print (next(dfs_paths(initial_config, goal_config)))
-
-"""print("calculating position...")
-moveDown(initial_config)
-for i in initial_config:
-    print(i)
-
-moveDown(initial_config)
-for i in initial_config:
-    print(i)
-
-moveDown(initial_config)
-for i in initial_config:
-    print(i)"""
