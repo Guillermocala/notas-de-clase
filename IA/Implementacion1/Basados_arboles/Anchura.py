798757ba54106109b9d8e76cfca1c5a9f4a67cb6
Guillermo Cala <guillermo.cala7@gmail.com>
2022-10-17 20:19:25 -0500

    aun sin poder trazar la ruta correcta...

--- IA/Implementacion1/Basados_arboles/Anchura.py
+++ IA/Implementacion1/Basados_arboles/Anchura.py
@@ -7,6 +7,41 @@ goal_config = [[1,2,3],
                [4,5,6],
                [7,8,0]]
 
+###########################################
+# Python3 program to check if a given
+# instance of 8 puzzle is solvable or not
+ 
+# A utility function to count
+# inversions in given array 'arr[]'
+def getInvCount(arr):
+    inv_count = 0
+    empty_value = -1
+    for i in range(0, 9):
+        for j in range(i + 1, 9):
+            if arr[j] != empty_value and arr[i] != empty_value and arr[i] > arr[j]:
+                inv_count += 1
+    return inv_count
+ 
+     
+# This function returns true
+# if given 8 puzzle is solvable.
+def isSolvable(puzzle) :
+ 
+    # Count inversions in given 8 puzzle
+    inv_count = getInvCount([j for sub in puzzle for j in sub])
+ 
+    # return true if inversion count is even.
+    return (inv_count % 2 == 0)
+     
+    # Driver code
+"""puzzle = [[8, 1, 2],[-1, 4, 3],[7, 6, 5]]
+if(isSolvable(puzzle)) :
+    print("Solvable")
+else:
+    print("Not Solvable")"""
+     
+    # This code is contributed by vitorhugooli
+##############################################
 def openFile(ruta):
     res = [[],[],[]]
     with open(ruta) as f:
@@ -24,7 +59,7 @@ def calculatePosition(actual):
                 position = (x, y)
                 return position
 
-def moveUp(actual_config):
+def moveDown(actual_config):
     # se usa deepcopy para que no altere la lista original
     res_config = copy.deepcopy(actual_config)
     actual_position = calculatePosition(res_config)
@@ -39,7 +74,7 @@ def moveUp(actual_config):
         res_config[x][y], res_config[x - 1][y] = res_config[x - 1][y], 0
         return res_config
 
-def moveRight(actual_config):
+def moveLeft(actual_config):
     # se usa deepcopy para que no altere la lista original
     res_config = copy.deepcopy(actual_config)
     actual_position = calculatePosition(res_config)
@@ -55,7 +90,7 @@ def moveRight(actual_config):
         return res_config
 
 
-def moveDown(actual_config):
+def moveUp(actual_config):
     # se usa deepcopy para que no altere la lista original
     res_config = copy.deepcopy(actual_config)
     actual_position = calculatePosition(res_config)
@@ -70,7 +105,7 @@ def moveDown(actual_config):
         res_config[x][y], res_config[x + 1][y] = res_config[x + 1][y], 0
         return res_config
 
-def moveLeft(actual_config):
+def moveRight(actual_config):
     # se usa deepcopy para que no altere la lista original
     res_config = copy.deepcopy(actual_config)
     actual_position = calculatePosition(res_config)
@@ -105,43 +140,56 @@ def expandCurrentState(actual_config):
 
     return sucesores
     
+
+
 def bfs_paths(start, goal):
-    visitados = []
-    queue = [(start, [start])]
+    visitados = [start]
+    queue = [start]
     while queue:
         print("el tamaño de visitados es: ", len(visitados))
-        print("len queue", len(queue))
+
         path = queue.pop(0)
-        print("len path", len(path))
-        print("path 0", path[0])
-        print("path 1", path[1])
+        #print(path)
         #print("el path antes de expandir es...")
         #print("path", path)
-        """for i in path:
-            print(i)"""
+        for i in path:
+            print(i)
 
-        if path[0] not in visitados:
-            sucesores = expandCurrentState(path[0])
-            visitados.append(path[0])
+        sucesores = expandCurrentState(path)
+        for vecino in sucesores:
+            if vecino not in visitados:
+                if vecino == goal:
+                    print("\t\tGOAL REACHED")
+                    """for x in vecino:
+                        print(x)
+                    visitados.append(vecino)"""
+                    return visitados
+                else:
+                    visitados.append(vecino)
+                    queue.append(vecino)
+
+        """if path not in visitados:
+            visitados.append(path)
             for i in sucesores:
                 if i == goal:
                     print("\t\tGOAL REACHED")
-                    return path
+                    return camino
                 else:
-                    #print("path + tuple", path + tuple(i))
-                    queue.append((i, path + tuple(i)))
-                    #queue.append(i, path + i)
+                    queue.append(i)
+                    camino.append(i)"""
 
 
 def main():
     initial_config = openFile("./configs/config.txt")
-    camino = bfs_paths(initial_config, goal_config)
-    print("len camino", len(camino))
-    print("len camino 0", len(camino[0]))
-    print("len camino 1", len(camino[1]))
-
-    for item in camino[0]:
-        print(item)
+    if(isSolvable(initial_config)) :
+        camino = bfs_paths(initial_config, goal_config)
+        #print("Camino", camino)
+    else:
+        print("No se puede solucionar!")
+    """for item in camino:
+        for x in item:
+            print(x)
+        print()"""
     """print("el camino tiene: ", len(camino[1]))
     for item in camino[1]:
         print(item)"""
