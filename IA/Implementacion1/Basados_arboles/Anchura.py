eff6659944b198adf733684c321353a4f8ca4d1e
Guillermo Cala <guillermo.cala7@gmail.com>
2022-10-18 00:42:29 -0500

    tengo el cerebro de adorno, quiero llorar...

--- IA/Implementacion1/Basados_arboles/Anchura.py
+++ IA/Implementacion1/Basados_arboles/Anchura.py
@@ -1,47 +1,18 @@
 # Taken from: https://eddmann.com/posts/depth-first-search-and-breadth-first-search-in-python/
 # a sample graph
 import copy
-import numpy as np
+
+"""en vez de usar un jodido sistema de array tridimensional
+es por mucho más optimo usar un sistema de string
+0123456789
+fue lo primero que pense solo para hacer los movimientos faciles
+al parecer tengo el cerebro de adorno...
+"""
 
 goal_config = [[1,2,3],
                [4,5,6],
                [7,8,0]]
 
-###########################################
-# Python3 program to check if a given
-# instance of 8 puzzle is solvable or not
- 
-# A utility function to count
-# inversions in given array 'arr[]'
-def getInvCount(arr):
-    inv_count = 0
-    empty_value = -1
-    for i in range(0, 9):
-        for j in range(i + 1, 9):
-            if arr[j] != empty_value and arr[i] != empty_value and arr[i] > arr[j]:
-                inv_count += 1
-    return inv_count
- 
-     
-# This function returns true
-# if given 8 puzzle is solvable.
-def isSolvable(puzzle) :
- 
-    # Count inversions in given 8 puzzle
-    inv_count = getInvCount([j for sub in puzzle for j in sub])
- 
-    # return true if inversion count is even.
-    return (inv_count % 2 == 0)
-     
-    # Driver code
-"""puzzle = [[8, 1, 2],[-1, 4, 3],[7, 6, 5]]
-if(isSolvable(puzzle)) :
-    print("Solvable")
-else:
-    print("Not Solvable")"""
-     
-    # This code is contributed by vitorhugooli
-##############################################
 def openFile(ruta):
     res = [[],[],[]]
     with open(ruta) as f:
@@ -140,22 +111,20 @@ def expandCurrentState(actual_config):
 
     return sucesores
     
-
-
 def bfs_paths(start, goal):
     visitados = [start]
     queue = [start]
+    ruta = [start]
+    expandidos = 1
     while queue:
-        print("el tamaño de visitados es: ", len(visitados))
-
+        print("La cantidad de expandidos es: ", expandidos)
+        expandidos += 1
         path = queue.pop(0)
-        #print(path)
-        #print("el path antes de expandir es...")
-        #print("path", path)
         for i in path:
             print(i)
 
         sucesores = expandCurrentState(path)
+
         for vecino in sucesores:
             if vecino not in visitados:
                 if vecino == goal:
@@ -163,29 +132,25 @@ def bfs_paths(start, goal):
                     """for x in vecino:
                         print(x)
                     visitados.append(vecino)"""
-                    return visitados
+                    return ruta + vecino
                 else:
                     visitados.append(vecino)
                     queue.append(vecino)
-
-        """if path not in visitados:
-            visitados.append(path)
-            for i in sucesores:
-                if i == goal:
-                    print("\t\tGOAL REACHED")
-                    return camino
-                else:
-                    queue.append(i)
-                    camino.append(i)"""
+                    ruta.append(path + vecino)
 
 
 def main():
     initial_config = openFile("./configs/config.txt")
-    if(isSolvable(initial_config)) :
-        camino = bfs_paths(initial_config, goal_config)
-        #print("Camino", camino)
+
+    camino = bfs_paths(initial_config, goal_config)
+    for x in camino:
+        print(x)
+
+    """if(isSolvable(initial_config)):
+
+        
     else:
-        print("No se puede solucionar!")
+        print("No se puede solucionar!")"""
     """for item in camino:
         for x in item:
             print(x)
